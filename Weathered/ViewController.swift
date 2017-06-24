f05ebc1144d4f41cc05833120205cd7bfdb0a509
Joe MacDonald <joem2525@gmail.com>
2017-06-24 04:52:28 -0400

    Initial Commit

---
+++ Weathered/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weathered
+//
+//  Created by lain on 2017-06-24.
+//  Copyright © 2017 joem. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
